9f3b13e26bc71652051cd6b3c109d873bf3be72d
sobino145 <gokr145@naver.com>
2022-07-01 09:25:09 +0900

    second commit

--- js/portfolio.js
+++ js/portfolio.js
@@ -217,7 +217,7 @@ new TypeIt("#typing2", {
 /* 기획영역 */
 $(".thumbs a").click(function(){
   let val=$(this).index();
-  let num=-1020*val+100;//이미지의 가로폭+여백20px=1020씩 누적
+  let num=-1020*val+20;//이미지의 가로폭+여백20px=1020씩 누적
   $("#gallery #largeImg").animate({"left":num});
   return false;
 });
